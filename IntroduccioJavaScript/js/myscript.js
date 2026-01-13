37bd988ec7aea2846ed040ec1ede8e54dd522f52
sgalrio0807 <sgalrio0807@g.educaand.es>
2026-01-13 10:22:35 +0100

    clase

---
+++ IntroduccioJavaScript/js/myscript.js
@@ -0,0 +1,42 @@
+//alert("Hello to JS");
+
+/*var table = "Normal Table";
+let chair = "One chair";
+console.log(table); 
+console.log(chair);
+
+tipo de variables
+let testBoolean = false;
+console.log(testBoolean);
+let testNumber = 10;
+console.log(testNumber);
+let testString = 'test';
+console.log(testString);
+
+Orientado a objetos
+let testBooleanObject = new Boolean(true);
+console.log(testBooleanObject);
+let testStringObject = new String('text');
+console.log(testStringObject);
+
+Concatenacion de variables
+let name = 'Jhon';
+let surname = 'Doe';
+let question = 'How are you ' +name+ ' ' +surname+ '?';
+console.log(question);
+
+let name = 'Jhon';
+let surname = 'Doe';
+let age = 23;
+let question = `How old is ${name} ${surname} ?`;
+let answer = 'He is ' +age+ ' years old';
+console.log(question);
+console.log(answer);
+
+let name = 'Jhon';
+let surname = 'Doe';
+let question = 'How are you ' .concat(name)+' ' .concat(surname)+ '?';
+console.log(question);*/
+
+
+
